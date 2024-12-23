a50d635f48178c89f78b4be9d2579613b2c7debf
xubinh <xubin.huang@outlook.com>
2024-12-23 15:59:50 +0800

    Write utility scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -x
 
-SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
-BUILD_TYPE=${BUILD_TYPE:-Debug}
+SOURCE_DIR="$(pwd)"
+BUILD_DIR=${BUILD_DIR:-./build} # e.g. `./build`
+BUILD_TYPE=${BUILD_TYPE:-Debug} # e.g. `Debug`
 
-mkdir -p $BUILD_DIR/$BUILD_TYPE \
-    && cd $BUILD_DIR/$BUILD_TYPE \
-    && cmake \
-            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-            $SOURCE_DIR \
-    && make $*
+mkdir -p "$BUILD_DIR/$BUILD_TYPE" # e.g. `./build/Debug`
+    
+cd "$BUILD_DIR/$BUILD_TYPE" || exit 1
+    
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE"  "$SOURCE_DIR"
+    
+make
